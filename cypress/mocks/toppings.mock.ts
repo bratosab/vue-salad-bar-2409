253f274a21d7af17b68e9baf924e048921e2cbce
Abdallah <bratosab@gmail.com>
2024-09-27 11:32:46 +0200

    feat(e2e): mock api requests

---
+++ cypress/mocks/toppings.mock.ts
@@ -0,0 +1,24 @@
+import type { Topping } from '../../src/models/Topping.model'
+
+export const toppingsMock: Topping[] = [
+  {
+    id: 1,
+    name: 'Cheese',
+    price: 2
+  },
+  {
+    id: 2,
+    name: 'Bacon',
+    price: 2
+  },
+  {
+    id: 3,
+    name: 'Pineapple',
+    price: 4
+  },
+  {
+    id: 4,
+    name: 'Tuna',
+    price: 2
+  }
+]
